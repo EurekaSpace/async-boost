9889cdbf7959f6014559dd66207c153888e5be97
ish <tmdghks0615@gmail.com>
2024-12-29 17:21:42 +0900

    init project

---
+++ src/main/kotlin/io/anan/eurekaspace/async_boost/AsyncBoostApplication.kt
@@ -0,0 +1,11 @@
+package io.anan.eurekaspace.async_boost
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsyncBoostApplication
+
+fun main(args: Array<String>) {
+	runApplication<AsyncBoostApplication>(*args)
+}
---
+++ src/test/kotlin/io/anan/eurekaspace/async_boost/AsyncBoostApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.anan.eurekaspace.async_boost
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AsyncBoostApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
