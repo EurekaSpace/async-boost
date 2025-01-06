f73cccb2023d515ff637222c2586fb583ceab489
ish <tmdhgks2515@idrsystems.co.kr>
2025-01-06 18:18:26 +0900

    fix: 패키지 구조 수정 (아케텍처 복잡도 감소를 위함) (3)

--- src/main/kotlin/io/anan/eurekaspace/async_boost/application/port/out/mq/ChatMqPort.kt
+++
@@ -1,7 +0,0 @@
-package io.anan.eurekaspace.async_boost.application.port.out.mq
-
-import io.anan.eurekaspace.async_boost.domain.model.ChatMessage
-
-interface ChatMqPort {
-    fun pubMessage(message: ChatMessage)
-}
--- src/main/kotlin/io/anan/eurekaspace/async_boost/application/port/out/persist/ChatPersistPort.kt
+++
@@ -1,7 +0,0 @@
-package io.anan.eurekaspace.async_boost.application.port.out.persist
-
-import io.anan.eurekaspace.async_boost.domain.model.ChatMessage
-
-interface ChatPersistPort {
-    fun saveMessage(message: ChatMessage)
-}
